a4a27d0408e978ce47ea70f17b4589476e52de99
IBecerra <becerraivan79@gmail.com>
2024-09-08 20:18:15 -0300

    componentes principales registrados

---
+++ src/app/componentes/elementos/login-registro/login-registro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-registro',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-registro.component.html',
+  styleUrl: './login-registro.component.scss'
+})
+export class LoginRegistroComponent {
+
+}
