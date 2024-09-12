a8e01156251175bea42050273467ead02c365e63
IBecerra <becerraivan79@gmail.com>
2024-09-11 22:26:40 -0300

    terminado sprint 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AhorcadoComponent } from "./componentes/juegos/ahorcado/ahorcado.component";
+import { InicioComponent } from "./componentes/paginas/inicio/inicio.component";
+import { LoginRegistroComponent } from './componentes/elementos/login-registro/login-registro.component';
+import { QuienSoyComponent } from './componentes/elementos/quien-soy/quien-soy.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InicioComponent, AhorcadoComponent, InicioComponent, LoginRegistroComponent, QuienSoyComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/componentes/paginas/inicio/inicio.component.ts
+++ src/app/componentes/paginas/inicio/inicio.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LoginRegistroComponent } from "../../elementos/login-registro/login-registro.component";
+import { QuienSoyComponent } from '../../elementos/quien-soy/quien-soy.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [LoginRegistroComponent, QuienSoyComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
